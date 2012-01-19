5af00aeeebdfcd3bf4f04fff103a2ab3f50d359b
Mariusz Kozakowski <11mariom+wordpress@gmail.com>
2012-01-19 20:09:29 +0100

    Better sed regex

--- ISPConfig3/auto_testdomain.sh
+++ ISPConfig3/auto_testdomain.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in $(ls -d /var/www/clients/*/*/web/); do
-ln -s $(echo $i) $(echo $i|sed 's:clients\/client.\/:test\/:'|sed 's:\/web\/::');
+ln -s $(echo $i) $(echo $i|sed -e 's/clients\/client[0-9]*\//test\//' -e 's/\/web\///');
 done
